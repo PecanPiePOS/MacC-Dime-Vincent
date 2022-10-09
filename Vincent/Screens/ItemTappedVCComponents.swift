c7a2d68efbb984ada5bc476aa336b66dd5ad14a7
Rookie0031 <jisu7408@gmail.com>
2022-10-10 08:59:53 +0900

    :sparkles: 버튼 색상, 폰트 수정

--- Vincent/Screens/ItemTappedVCComponents.swift
+++ Vincent/Screens/ItemTappedVCComponents.swift
@@ -15,19 +15,23 @@ let testLabel = UILabel().then {
     $0.text = "Image would be here"
 }
 
+let bottomUIView = UIView(frame: .zero).then {
+    $0.backgroundColor = .mainBlack
+}
+
 let symbolConfiguration = UIImage.SymbolConfiguration(textStyle: .title1)
 
 let likeButton = UIButton().then {
     let image = UIImage(systemName: "heart", withConfiguration: symbolConfiguration)
     $0.setImage(image, for: .normal)
+    $0.tintColor = .white
 }
 
-let bottomUIView = UIView(frame: .zero).then {
-    $0.backgroundColor = .gray
-}
-
-let buyButton = UIButton(configuration: .filled()).then {
+// MARK: 추후 setTitle부분에 변수가 들어가야함
+let buyButton = UIButton().then {
     $0.setTitle("33000원에 살게요", for: .normal)
+    $0.titleLabel?.font = UIFont.systemFont(ofSize: 14, weight: .bold)
     $0.setTitleColor(.black, for: .normal)
-    $0.tintColor = .systemYellow
+    $0.backgroundColor = .mainYellow
+    $0.layer.cornerRadius = 15
 }
