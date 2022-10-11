35313d30e7e58875777aefd28a1b59cf03d31381
PecanPie <PecanPieMail>
2022-10-11 11:13:13 +0900

    :lipstick: TextView UI 구현

---
+++ Vincent/Screens/Sell/SellPhotoAddedBox.swift
@@ -0,0 +1,20 @@
+//
+//  SellPhotoBox.swift
+//  Vincent
+//
+//  Created by KYUBO A. SHIM on 2022/10/11.
+//
+
+import UIKit
+
+class SellPhotoAddedBox: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Vincent/Screens/Sell/SellPhotoTapAddBox.swift
@@ -0,0 +1,20 @@
+//
+//  SellPhotoTapAddBox.swift
+//  Vincent
+//
+//  Created by KYUBO A. SHIM on 2022/10/11.
+//
+
+import UIKit
+
+class SellPhotoTapAddBox: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
